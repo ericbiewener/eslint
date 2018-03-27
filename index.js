455c24b00ec7f2e6dade79257a860b7065d6ddc7
Eric Biewener <eric@talentpair.com>
2018-03-27 08:28:01 -0700

    Fix directory structure, remove comma-dangle

---
+++ index.js
@@ -0,0 +1,34 @@
+module.exports = {
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'sourceType': 'module'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2,
+      {SwitchCase: 1}
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'no-console': 0,
+    'prefer-const': 2,
+    'object-curly-spacing': [
+      'error',
+      'never'
+    ]
+  }
+}
