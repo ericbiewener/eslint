8cc1947d4e5e86e0910b05a26f5af427bf9d7344
Eric Biewener <ebiewener@gmail.com>
2019-02-23 15:55:37 -0800

    Update

--- base/index.js
+++ base/index.js
@@ -1,28 +1,28 @@
-module.exports = {
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
-  parser: 'babel-eslint',
+exports = {
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parser: "babel-eslint",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 2017,
-    sourceType: 'module',
+    sourceType: "module"
   },
   rules: {
-    indent: ['error', 2, { SwitchCase: 1 }],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single', { avoidEscape: true }],
-    semi: ['error', 'never'],
-    'no-console': 0,
-    'prefer-const': 2,
-    'object-curly-spacing': ['error', 'always'],
-    'comma-dangle': ['error', 'always-multiline'],
-    'no-cond-assign': 0,
-    'no-throw-literal': 2,
-    'no-unused-vars': ['error', { ignoreRestSiblings: true }],
-    'no-unused-expression': 2,
+    indent: ["error", 2, { SwitchCase: 1 }],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single", { avoidEscape: true }],
+    semi: ["error", "never"],
+    "no-console": 0,
+    "prefer-const": 2,
+    "object-curly-spacing": ["error", "always"],
+    "comma-dangle": ["error", "always-multiline"],
+    "no-cond-assign": 0,
+    "no-throw-literal": 2,
+    "no-unused-vars": ["error", { ignoreRestSiblings: true }],
+    "no-unused-expression": 2,
 
     // React
-    'react/prop-types': 0,
-  },
-}
+    "react/prop-types": 0
+  }
+};
