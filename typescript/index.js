abc6eb17166d9cb47f6b4d038a5976a328b56a23
Eric Biewener <ebiewener@gmail.com>
2019-02-23 15:50:43 -0800

    Add babel-eslint to typescript dependencies

--- typescript/index.js
+++ typescript/index.js
@@ -1,31 +1,8 @@
-module.exports = {
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-  ],
-  parser: 'babel-eslint',
+exports = {
+  extends: ["plugin:@typescript-eslint/recommended", "ericbiewener"],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2017,
-    sourceType: 'module',
-  },
-  rules: {
-    indent: ['error', 2, { SwitchCase: 1 }],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single', { avoidEscape: true }],
-    semi: ['error', 'never'],
-    'no-console': 0,
-    'prefer-const': 2,
-    'object-curly-spacing': ['error', 'always'],
-    'comma-dangle': ['error', 'always-multiline'],
-    'no-cond-assign': 0,
-    'no-throw-literal': 2,
-    'no-unused-vars': ['error', { ignoreRestSiblings: true }],
-    'no-unused-expression': 2,
-
-    // React
-    'react/prop-types': 0,
-  },
-}
+    project: "./tsconfig.json"
+  }
+};
