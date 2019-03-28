a0127c1d8337e733a1d92a63da2f3cc14c0e34a3
Eric Biewener <ebiewener@gmail.com>
2019-03-28 09:46:43 -0700

    Use module.exports

--- typescript/index.js
+++ typescript/index.js
@@ -1,8 +1,12 @@
-exports = {
-  extends: ["plugin:@typescript-eslint/recommended", "ericbiewener"],
-  plugins: ["@typescript-eslint"],
-  parser: "@typescript-eslint/parser",
+module.exports = {
+  extends: ['plugin:@typescript-eslint/recommended', 'ericbiewener'],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: "./tsconfig.json"
-  }
-};
+    project: './tsconfig.json',
+  },
+  rules: {
+    indent: 'off',
+    '@typescript-eslint/indent': ['error', 2],
+  },
+}
