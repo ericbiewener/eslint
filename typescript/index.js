60301f8eece0fabb58173b65f9c42d8e2040a117
Eric Biewener <ebiewener@gmail.com>
2019-03-28 19:41:51 -0700

    Fixes for prettier

--- typescript/index.js
+++ typescript/index.js
@@ -1,12 +1,24 @@
 module.exports = {
-  extends: ['plugin:@typescript-eslint/recommended', 'ericbiewener'],
-  plugins: ['@typescript-eslint'],
-  parser: '@typescript-eslint/parser',
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "ericbiewener",
+    "prettier/@typescript-eslint"
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: './tsconfig.json',
+    project: "./tsconfig.json"
   },
   rules: {
-    indent: 'off',
-    '@typescript-eslint/indent': ['error', 2],
-  },
-}
+    indent: "off",
+    "no-unused-expression": 0,
+    "no-use-before-define": 0,
+    "no-unused-vars": 0,
+    "@typescript-eslint/indent": ["error", 2],
+    "@typescript-eslint/no-unused-vars": 0,
+    "@typescript-eslint/no-use-before-define": [
+      2,
+      { functions: false, classes: false }
+    ]
+  }
+};
