3f94c7c5887ee010fad6c04b9f421caedff2a525
Eric Biewener <ebiewener@gmail.com>
2019-11-02 14:34:28 -0700

    Backtick quotes

--- base/index.js
+++ base/index.js
@@ -1,29 +1,29 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
-  parser: 'babel-eslint',
+  extends: [`eslint:recommended`, `plugin:react/recommended`, `prettier`],
+  parser: `babel-eslint`,
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2017,
-    sourceType: 'module',
+    sourceType: `module`,
   },
   rules: {
-    indent: ['error', 2, { SwitchCase: 1 }],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single', { avoidEscape: true }],
-    semi: ['error', 'never'],
-    'no-console': 'off',
-    'prefer-const': 'error',
-    'object-curly-spacing': ['error', 'always'],
-    'comma-dangle': ['error', 'always-multiline'],
-    'no-cond-assign': 'off',
-    'no-throw-literal': 'error',
-    'no-unused-vars': ['error', { ignoreRestSiblings: true }],
-    'no-unused-expressions': 'error',
-    'no-unexpected-multiline': 'error',
-    'max-len': [
-      'error',
+    indent: [`error`, 2, { SwitchCase: 1 }],
+    "linebreak-style": [`error`, `unix`],
+    quotes: [`error`, `backtick`],
+    semi: [`error`, `never`],
+    "no-console": `off`,
+    "prefer-const": `error`,
+    "object-curly-spacing": [`error`, `always`],
+    "comma-dangle": [`error`, `always-multiline`],
+    "no-cond-assign": `off`,
+    "no-throw-literal": `error`,
+    "no-unused-vars": [`error`, { ignoreRestSiblings: true }],
+    "no-unused-expressions": `error`,
+    "no-unexpected-multiline": `error`,
+    "max-len": [
+      `error`,
       {
         code: 100,
         ignoreTrailingComments: true,
@@ -35,21 +35,21 @@ module.exports = {
     ],
 
     // React
-    'react/prop-types': 0,
+    "react/prop-types": 0,
   },
   overrides: [
     {
-      files: ['**/*.test.{ts,tsx,js,jsx}', '**/testUtils.{ts,tsx,js,jsx}'],
+      files: [`**/*.test.{ts,tsx,js,jsx}`, `**/testUtils.{ts,tsx,js,jsx}`],
       env: {
         jest: true,
       },
       rules: {
-        'no-empty': 'off',
-        'max-nested-callbacks': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-object-literal-type-assertion': 'off',
-        '@typescript-eslint/no-non-null-assertion': 'off',
-        '@typescript-eslint/ban-ts-ignore': 'off',
+        "no-empty": `off`,
+        "max-nested-callbacks": `off`,
+        "@typescript-eslint/no-explicit-any": `off`,
+        "@typescript-eslint/no-object-literal-type-assertion": `off`,
+        "@typescript-eslint/no-non-null-assertion": `off`,
+        "@typescript-eslint/ban-ts-ignore": `off`,
       },
     },
   ],
